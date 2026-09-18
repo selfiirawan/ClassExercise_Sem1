fcb64c3be042d43fa4abbccfa2218169a13999e1
selfiirawan <celpi.irawan@gmail.com>
2026-09-18 23:31:35 +0800

    modified git ignore and js exercise

--- extra_class.js
+++ extra_class.js
@@ -1,9 +1,9 @@
-const nums = [3,8,15,42,7];
+const numbers = [3, 8, 15, 42, 7];
 
-nums.forEach(function(n) {
-    if (n % 2 === 0) {
-        console.log(`${n} is even`);
+numbers.forEach(function(num) {
+    if (num % 2 === 0) {
+        console.log(`${num} is even`);
     } else {
-        console.log(`${n} is odd`);
+        console.log(`${num} is odd`);
     }
 });
