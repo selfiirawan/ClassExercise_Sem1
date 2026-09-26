bfebb8c8fd2a9c3533e96b7f2bd95a78e88895a0
selfiirawan <celpi.irawan@gmail.com>
2026-09-26 16:17:41 +0800

    mock PA

--- SEM 1/rbac-exercise/config/database.php
+++ SEM 1/rbac-exercise/config/database.php
@@ -8,3 +8,8 @@ $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $passw
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 // echo "Connected Successfully!";
+
+
+
+
+
